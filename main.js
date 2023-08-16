1921ca283a0eaf7b82e5396dbbc9621f458c30d5
dakoo096 <noelia_cabral96@hotmail.com>
2023-08-16 19:04:04 -0300

    se agrega animacion a los elementos de la pagina,se agrega vista de las imagenes del carrousel

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+AOS.init({
+  duration: 1200,
+})
+
+
 let mybutton = document.getElementById("myBtn");
 
 // cuando se scrollea para abajo aparece el boton
@@ -29,4 +34,51 @@ function topFunction() {
     btnSwitch.classList.toggle('active');
   });   
 
+  
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const overlay = document.getElementById("overlay");
+    const closeBtn = document.getElementById("closeBtn");
+    const overlayImg = document.getElementById("overlayImg");
+    const overlayPrev = document.getElementById("overlayPrev");
+    const overlayNext = document.getElementById("overlayNext");
+    const projectImages = document.querySelectorAll(".containerOrquideario article img");
+  
+    let currentIndex = 0;
+  
+    projectImages.forEach((img, index) => {
+      img.addEventListener("click", () => {
+        overlay.style.display = "flex";
+        currentIndex = index;
+        updateOverlay();
+      });
+    });
+  
+    function updateOverlay() {
+      overlayImg.src = projectImages[currentIndex].src;
+    }
+  
+    function showNextImage() {
+      if (currentIndex < projectImages.length - 1) {
+        currentIndex++;
+        updateOverlay();
+      }
+    }
+  
+    function showPrevImage() {
+      if (currentIndex > 0) {
+        currentIndex--;
+        updateOverlay();
+      }
+    }
+  
+    closeBtn.addEventListener("click", () => {
+      overlay.style.display = "none";
+    });
+  
+    overlayPrev.addEventListener("click", showPrevImage);
+    overlayNext.addEventListener("click", showNextImage);
+  });
   
